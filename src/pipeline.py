9554bbf6de78a09e0c25d0dcf15d253c22fb9bb5
Pedro Vasconcelos <pedrovasconcelos.henrique@gmail.com>
2026-01-17 20:01:15 -0300

    task(bronze): cria asset extract_auction_data (closes #4)

--- src/pipeline.py
+++ src/pipeline.py
@@ -40,6 +40,36 @@ def get_realm_id(context: AssetExecutionContext):
 
     return data
 
+@asset
+def extract_auction_data(context: AssetExecutionContext):
+    auth = BlizzardAuth()
+    token = auth.get_token()
+    
+    connected_realm_id = 3209
+
+    url = f"https://us.api.blizzard.com/data/wow/connected-realm/{connected_realm_id}/auctions"
+
+    query_params = {
+        'namespace': 'dynamic-us',
+        'locale': 'pt_BR'
+    }
+
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+    
+    context.log.info(f"Baixando dados do leilão para Realm ID: {connected_realm_id}...")
+
+    response = requests.get(url, params=query_params, headers=headers)
+    response.raise_for_status()
+
+    data = response.json()
+
+    qtd_itens = len(data.get('auctions', []))
+    context.log.info(f"Download concluído. Total de lotes no leilão: {qtd_itens}")
+    
+    return data
+
 defs = Definitions(
-    assets = [get_token, get_realm_id]
+    assets = [get_token, get_realm_id, extract_auction_data]
 )
